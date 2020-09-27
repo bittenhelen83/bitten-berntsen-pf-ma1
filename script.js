7d68c1418caacf2da8a78b61e870a65bb676386b
bittenhelen83 <65275228+bittenhelen83@users.noreply.github.com>
2020-09-27 16:23:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//question 1
+var firstVar
+
+//question 2
+var firstName =  "Bitten"
+
+//question 3
+var number1 = 15
+
+//question 4
+var division = 20/5
+
+//question 5
+var animal = "frog"
+console.log(animal === "frog")
+
+//question 6
+var orderHasShipped = "true";
+
+//question 7
+if(orderHasShipped === "true") {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+//question 8
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
